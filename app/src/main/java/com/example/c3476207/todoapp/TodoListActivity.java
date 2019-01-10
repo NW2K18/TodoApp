824f340813c47a42c6dd40d6c612eadc96535ff4
nathan <nathw014@gmail.com>
2019-01-10 04:13:36 +0000

    Created activity_fragment A new view xml file

--- app/src/main/java/com/example/c3476207/todoapp/TodoListActivity.java
+++ app/src/main/java/com/example/c3476207/todoapp/TodoListActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 public class TodoListActivity extends AppCompatActivity {
 
     @Override
+    // OnCreate method is neccessary for android programs
+    // Uses saved instance state to handle rotation problem
+    // Sets content view to to-do list
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
